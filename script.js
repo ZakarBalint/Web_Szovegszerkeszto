3e79233e3fcdc6204e160e66ea029a0df033ce83
ZakarBalint <szt2zakbal@vasvari.org>
2024-01-25 13:30:54 +0100

    asd

--- script.js
+++ script.js
@@ -1,16 +1,53 @@
-const inputFilePicker = document.getElementById("filePicker");
-const inputTextEditor = document.getElementById("textEditor");
+const filePicker = document.getElementById("filePicker");
+const TextEditor = document.getElementById("textEditor");
 
-inputFilePicker.onchange = function loadFileInTextEditor()
+TextEditor.value = "";
+filePicker.value = null;
+
+filePicker.onchange = function loadFileInTextEditor()
 {
-    var file = inputFilePicker.files[0];
+    var file = filePicker.files[0];
 
     var reader = new FileReader();
     reader.readAsText(file, "UTF-8");
     reader.onload = function (evt) {
-        document.getElementById("fileContents").innerHTML = evt.target.result;
+        TextEditor.value = evt.target.result;
     }
     reader.onerror = function (evt) {
-        document.getElementById("fileContents").innerHTML = "error reading file";
+        alert("Hiba a fájl beolvasásában!");
     }
+}
+
+const buttonSave = document.getElementById("buttonSave");
+const buttonSaveAs = document.getElementById("buttonSaveAs");
+
+buttonSave.onclick = function SaveFile()
+{
+    var newFile = new Blob([TextEditor.value], {type: 'text/plain'});
+
+    window.URL = window.URL || window.webkitURL;
+    var dlBtn = document.getElementById("download");
+
+    dlBtn.setAttribute("href", window.URL.createObjectURL(newFile));
+    dlBtn.setAttribute("download", filePicker.files[0].name);
+}
+
+
+buttonSaveAs.onclick = function SaveFileAs()
+{
+    let fileName = prompt("Kérem adja meg a fájl nevét:");
+
+    if(fileName == null || fileName == "")
+    {
+        alert("A fájlnak nincs neve!")
+        return;
+    }
+
+    var newFile = new Blob([TextEditor.value], {type: 'text/plain'});
+
+    window.URL = window.URL || window.webkitURL;
+    var dlBtn = document.getElementById("download");
+
+    dlBtn.setAttribute("href", window.URL.createObjectURL(newFile));
+    dlBtn.setAttribute("download", fileName);
 }
